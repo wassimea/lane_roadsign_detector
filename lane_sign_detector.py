3f91f5867538232c5fa2fea3dd545c1aae926a1e
Wassim El Ahmar <w.elahmar@hotmail.com>
2021-07-06 11:11:22 -0400

    Added debug assistants

--- lane_sign_detector.py
+++ lane_sign_detector.py
@@ -8,6 +8,10 @@ def region(img,vertices):
 	mask = np.zeros_like(img)
 	match_mask_color=  255   
 	cv2.fillPoly(mask,vertices,match_mask_color)
+
+	#cv2.imshow("mask", mask)
+	#cv2.waitKey()
+
 	masked_image=cv2.bitwise_and(img,mask)
 	return masked_image
 
@@ -17,12 +21,15 @@ def draw_the_lines(img,lines):
 
     if lines is not None:
         for line in lines:
-            for x1,y1,x2,y2 in line:
-                slope  = abs((y2 - y1) / (x2 - x1))
-                if slope > 0.30:
-                    cv2.line(blank_image,(x1,y1),(x2,y2),(0,255,0),thickness=3)
+            x1, y1, x2, y2 = line[0]
+            slope  = abs((y2 - y1) / (x2 - x1))
+            if slope > 0.30:
+                cv2.line(blank_image,(x1,y1),(x2,y2),(0,255,0),thickness=3)
 
-        imge = cv2.addWeighted(imge,0.8,blank_image,1,0.0)
+        #cv2.imshow("blank_image", blank_image)
+        #cv2.waitKey()
+
+        imge = cv2.addWeighted(imge,1.0,blank_image,1,gamma=0.0)
     return imge
 
 def check_angles(shape, img):
@@ -32,6 +39,18 @@ def check_angles(shape, img):
     p2x, p2y = p2[0]
     p3x, p3y = p3[0]
     p4x, p4y = p4[0]
+
+    #img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    #cv2.circle(img, (p1x,p1y), radius=1, color=(0, 0, 255), thickness=-1)
+    #cv2.circle(img, (p2x,p2y), radius=1, color=(0, 0, 255), thickness=-1)
+    #cv2.circle(img, (p3x,p3y), radius=1, color=(0, 0, 255), thickness=-1)
+    #cv2.circle(img, (p4x,p4y), radius=1, color=(0, 0, 255), thickness=-1)
+    #cv2.putText(img,"p1", (p1x,p1y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    #cv2.putText(img,"p2", (p2x,p2y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    #cv2.putText(img,"p3", (p3x,p3y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    #cv2.putText(img,"p4", (p4x,p4y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, 255)
+    #cv2.imshow("img", img)
+    #cv2.waitKey()
 
     a1r = math.atan2(p4y - p1y, p4x - p1x) - math.atan2(p2y - p1y, p2x - p1x)
     a2r = math.atan2(p3y - p4y, p3x - p4x) - math.atan2(p1y - p4y, p1x - p4x)
@@ -68,19 +87,26 @@ def get_mser(mser, image):
     height, width, _ = img.shape
     roi = img[0:700, int(width/2):]
 
+    #cv2.imshow("roi", roi)
+    #cv2.waitKey()
+
     roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
     rects = []
     regions = mser.detectRegions(roi)
     hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions[0]]
+
+    #cv2.polylines(roi, hulls, 1, (0, 255, 0))
+    #cv2.imshow("roi", roi)
+    #cv2.waitKey()
+    
     for hull in hulls:
-        peri = cv2.arcLength(hull, True)
-        approx = cv2.approxPolyDP(hull, 0.005*peri, True)
+        peri = cv2.arcLength(hull, True)    #approximate perimeter
+        approx = cv2.approxPolyDP(hull, 0.005*peri, closed=True)    
         if len(approx) == 4:
             (x, y, w, h) = cv2.boundingRect(approx)
             if w >50 and h > 50:
-                if check_angles(approx, roi):
-                
+                if check_angles(approx, roi): 
                     rects.append([int(width/2) + x,y,int(width/2) + x+w,y+h])
     return rects
 
@@ -107,8 +133,16 @@ def process_images(folder_path):
         region_of_interest_coor = [ low_left,mid,bottom_right]
 
         edges = cv2.Canny(frame,80,200)
+        
+        #cv2.imshow("edges", edges)
+        #cv2.waitKey()
+        
         isolated = region(edges, np.array([region_of_interest_coor],np.int32))
-        lines = cv2.HoughLinesP(isolated, 1, np.pi/180, 40, np.array([]), minLineLength=50, maxLineGap=50)
+
+        #cv2.imshow("isolated", isolated)
+        #cv2.waitKey()
+
+        lines = cv2.HoughLinesP(isolated, 1, np.pi/180, 40, minLineLength=50, maxLineGap=50)
 
 
         image_with_lines = draw_the_lines(frame,lines)
@@ -128,7 +162,7 @@ def process_images(folder_path):
 
 
 def main():
-    process_images("/media/wassimea/Storage/invest_ottawa/lane-detector/images/")
+    process_images("/home/wassimea/Desktop/kanata_images/")
 
 
 
